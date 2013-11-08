fc9dcce7947b2308c38105b9a217502dd4f7f884
Ciprian Dorin Craciun <ciprian@volution.ro>
2013-11-08 21:22:10 +0200

    Import scripts and submodules from `mosaic-distribution` repository

---
+++ scripts/_env.bash
@@ -0,0 +1,24 @@
+#!/dev/null
+
+set -e -E -u -o pipefail -o noclobber -o noglob +o braceexpand || exit 1
+trap 'printf "[ee] failed: %s\n" "${BASH_COMMAND}" >&2' ERR || exit 1
+export -n BASH_ENV
+
+_workbench="$( readlink -e -- . )"
+_repositories="${_workbench}/repositories"
+_scripts="${_workbench}/.scripts"
+_tools="${_workbench}/.tools"
+
+_PATH="${_tools}/bin:${_PATH}"
+
+_git_bin="$( PATH="${_PATH}" type -P -- git || true )"
+if test -z "${_git_bin}" ; then
+	echo "[ww] missing \`git\` (Git DSCV) executable in path: \`${_PATH}\`; ignoring!" >&2
+	_git_bin=git
+fi
+
+_git_args=()
+_git_env=()
+while read _git_env_var ; do
+	_git_env+=( "${_git_env_var}" )
+done < <( env )
---
+++ scripts/reset.bash
@@ -0,0 +1,18 @@
+#!/dev/null
+
+if ! test "${#}" -eq 0 ; then
+	echo "[ee] invalid arguments; aborting!" >&2
+	exit 1
+fi
+
+echo "[ii] resetting repositories..." >&2
+env -i "${_git_env[@]}" "${_git_bin}" \
+		submodule foreach --quiet --recursive \
+			"${_git_bin} reset -q --hard HEAD"
+
+echo "[ii] cleaning repositories..." >&2
+env -i "${_git_env[@]}" "${_git_bin}" \
+		submodule foreach --quiet --recursive \
+			"${_git_bin} clean -q -f -d -x"
+
+exit 0
